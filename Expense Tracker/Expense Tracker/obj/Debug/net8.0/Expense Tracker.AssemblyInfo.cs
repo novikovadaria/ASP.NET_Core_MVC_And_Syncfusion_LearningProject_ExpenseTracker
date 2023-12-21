804c0177df2cc866fd78b909a09b9b02357b9262
79384 <smb_udk@vk.com>
2023-12-21 21:10:20 +0700

    sidebar menu is done

--- Expense Tracker/Expense Tracker/obj/Debug/net8.0/Expense Tracker.AssemblyInfo.cs
+++ Expense Tracker/Expense Tracker/obj/Debug/net8.0/Expense Tracker.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Expense Tracker")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+86ef90aa6b33737a7dfe3978c2d630707b4db72b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+65c5b8c0950dd8593158097fd7b2702d757e04f9")]
 [assembly: System.Reflection.AssemblyProductAttribute("Expense Tracker")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Expense Tracker")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
